08f9b7ead4bab75a492ad8cb7f6393460b2009c9
Ali Hamza <alihamzaofficial3536@gmail.com>
2024-11-30 20:46:27 +0500

    updates

--- index.js
+++ index.js
@@ -1,62 +1,79 @@
+import 'dotenv/config';
 import puppeteer from 'puppeteer-core';
-import { CreateProfile, startBrowser } from './utils/functions.js';
+import fs from 'fs';
+import { between, CreateProfile, DeleteProfile, startBrowser, wait } from './utils/functions.js';
 
-let input = [
-    {
-        keyword: "Neckless",
-        location: "New York",
-        personalization: "RED",
-        listing_url: "https://www.etsy.com/listing/851979574/attached-diamond-on-chain-14kt-gold?click_key=03ec415e4d36914ea03990f019dc5a0e9db54bb6%3A851979574&click_sum=02498447&ref=search2_top_narrowing_intent_modules_etsys_pick-1&pro=1&frs=1&sts=1"
-    },
-    {
-        keyword: "Neckless",
-        location: "New York",
-        personalization: "RED",
-        listing_url: "https://www.etsy.com/listing/851979574/attached-diamond-on-chain-14kt-gold?click_key=03ec415e4d36914ea03990f019dc5a0e9db54bb6%3A851979574&click_sum=02498447&ref=search2_top_narrowing_intent_modules_etsys_pick-1&pro=1&frs=1&sts=1"
-    },
-    {
-        keyword: "Neckless",
-        location: "New York",
-        personalization: "RED",
-        listing_url: "https://www.etsy.com/listing/851979574/attached-diamond-on-chain-14kt-gold?click_key=03ec415e4d36914ea03990f019dc5a0e9db54bb6%3A851979574&click_sum=02498447&ref=search2_top_narrowing_intent_modules_etsys_pick-1&pro=1&frs=1&sts=1"
-    },
-    {
-        keyword: "Neckless",
-        location: "New York",
-        personalization: "RED",
-        listing_url: "https://www.etsy.com/listing/851979574/attached-diamond-on-chain-14kt-gold?click_key=03ec415e4d36914ea03990f019dc5a0e9db54bb6%3A851979574&click_sum=02498447&ref=search2_top_narrowing_intent_modules_etsys_pick-1&pro=1&frs=1&sts=1"
-    },
-    {
-        keyword: "Neckless",
-        location: "New York",
-        personalization: "RED",
-        listing_url: "https://www.etsy.com/listing/851979574/attached-diamond-on-chain-14kt-gold?click_key=03ec415e4d36914ea03990f019dc5a0e9db54bb6%3A851979574&click_sum=02498447&ref=search2_top_narrowing_intent_modules_etsys_pick-1&pro=1&frs=1&sts=1"
-    }
-],
-    LAST_STEP_LISTING_URL = 'https://www.etsy.com/featured/home-design-and-organization?ref=market_guides_carousel_2';
-
-(async () => {
-    
-    let PROFILE_ID = await CreateProfile({
-        name: "My Profile",
-        group_id: 5346181
-    });
-
-    let ws = await startBrowser(PROFILE_ID),
-        browser = await puppeteer.connect({
-            headless: false,
-            browserWSEndpoint: ws
-        }),
-        page = await browser.newPage();
-
-    await page.goto("https://etsy.com", { waitUntil: "networkidle0", timeout: 0 });
-
-    await page.type("#global-enhancements-search-query", keyword, { delay: 100 }); // Type Keyword
-    await page.keyboard.press("Enter");
-
-    await page.waitForNavigation({
+// Inputs 
+const INPUTS = JSON.parse(fs.readFileSync("inputs.json", "utf-8")),
+    SITE_URL = "https://etsy.com",
+    PAGE_OPTIONS = {
         waitUntil: "networkidle0",
         timeout: 0
+    };
+
+
+(async () => {
+
+    //#region (Creating Profile, Start Browser)
+    // Create Profile
+    let PROFILE_ID = await CreateProfile({
+        name: process.env.PROFILE_NAME,
+        group_id: process.env.GROUP_ID
     });
+
+    // Start Browser
+    let ws = await startBrowser(PROFILE_ID);
+
+    if (!ws) {
+        await DeleteProfile(PROFILE_ID);
+        console.log("Please Try Again!");
+        return false;
+    }
+
+    let browser = await puppeteer.connect({
+        headless: false,
+        browserWSEndpoint: ws
+    });
+    //#endregion (Creating Profile, Start Browser) 
+
+    for (let i = 0; i < INPUTS.length; i++) {
+        let { keyword, location, personalization, listing_url } = INPUTS[i],
+            page = await browser.newPage();
+
+        try {
+            await page.goto(SITE_URL, PAGE_OPTIONS);
+        } catch (error) {
+            continue;
+        }
+
+
+        // (Type Keyword, Show listings)
+        await page.type("#global-enhancements-search-query", keyword, { delay: 100 });
+        await page.keyboard.press("Enter");
+        await page.waitForNavigation({ timeout: 0 });
+
+
+        //#region Filters
+        await page.click("#search-filter-button");
+        await wait(3000);
+        await page.type("#shop-location-input", location, { delay: 500 });
+        await wait(4000);
+
+        let valid = await page.evaluate(() => {
+            let listbox = document.querySelector('#shop-location-input').getAttribute('aria-controls'),
+                listBoxCon = document.querySelector(`ul[id="${listbox}"]`);
+            if (!listBoxCon.firstChild) return false
+            listBoxCon.firstChild?.dispatchEvent(new Event("click"));
+            return true;
+        });
+        if (!valid) {
+            console.log("Location not found in filters so we will skip this listing");
+            continue;
+        }
+        await wait(3000);
+        await page.click(".search-filters-modal button[aria-label='Close']"); // Close Filters Sidebar
+        //#endregion Filters 
+
+    }
 
 })();
--- utils/functions.js
+++ utils/functions.js
@@ -1,43 +1,126 @@
 import axios from "axios";
-const API_URL = 'http://local.adspower.net:50325/api/v1/';
+import 'dotenv/config';
+// Wait Fn
+const wait = (ms) => new Promise((res, rej) => setTimeout(res, ms));
 
+// Get Random Number Between
+function between(from, to) {
+    return Math.floor(Math.random() * (to - from + 1)) + from;
+}
+// Merge Path
+function mergePath(...paths) {
+    let url = '';
+    paths.forEach(path => {
+        path = trim(path);
+        path = trim(path, '/');
+        if (path.length) url += `/${path}`;
+    });
+    url = trim(url, '/');
+    return url;
+}
+// Trim
+function trim(str, charlist) {
+    let whitespace = [' ', '\n', '\r', '\t', '\f', '\x0b', '\xa0', '\u2000', '\u2001', '\u2002', '\u2003', '\u2004', '\u2005', '\u2006', '\u2007', '\u2008', '\u2009', '\u200a', '\u200b', '\u2028', '\u2029', '\u3000'].join('')
+    let l = 0
+    let i = 0
+    str += ''
+    if (charlist) {
+        whitespace = (charlist + '').replace(/([[\]().?/*{}+$^:])/g, '$1')
+    }
+    l = str.length
+    for (i = 0; i < l; i++) {
+        if (whitespace.indexOf(str.charAt(i)) === -1) {
+            str = str.substring(i)
+            break
+        }
+    }
+    l = str.length
+    for (i = l - 1; i >= 0; i--) {
+        if (whitespace.indexOf(str.charAt(i)) === -1) {
+            str = str.substring(0, i + 1)
+            break
+        }
+    }
+    return whitespace.indexOf(str.charAt(0)) === -1 ? str : ''
+}
+
+// API_URL
+function ApiUrl(url) {
+    return mergePath(process.env.API_URL, url);
+}
+
+//#region Scraper Functions
 
 // Create Profile
 function CreateProfile(data = {}) {
     return new Promise(async (res, rej) => {
-        let { group_id, name, domain_name = '', proxyid = 'random' } = data;
+        let { group_id, name } = data;
 
         try {
-            let response = await axios.post(`${API_URL}user/create`, {
+            let response = await axios.post(ApiUrl('user/create'), {
                 name,
-                domain_name,
-                proxyid,
+                proxyid: between(1, process.env.MAX_PROXIES),
                 group_id
             });
+
             res(response.data.data.id);
         } catch (error) {
             console.log("Try Again Failed to make profile");
-
             res(false);
         }
 
     });
 
+}
+
+// Delete Profile
+function DeleteProfile(id) {
+    return new Promise(async (res, rej) => {
+
+        for (let i = 0; i < 3; i++) {
+            try {
+                let response = await axios.post(ApiUrl('user/delete'), {
+                    user_ids: [id],
+                });
+                response = response.data;
+                if (response.msg == 'success') break;
+            } catch (error) {
+                continue;
+            }
+        }
+
+        res(true);
+    });
 }
 
 // Start Browser
 function startBrowser(profile_id) {
     return new Promise(async (res, rej) => {
-        try {
-            let response = await axios.get(`${API_URL}browser/start?user_id=${profile_id}`);
-            res(response.data.data.ws.puppeteer);
-        } catch (error) {
-            console.log("Try Again Failed to launch browser");
-            res(false);
+        let puppeteer = false;
+
+        for (let i = 0; i < 3; i++) {
+            try {
+                let response = await axios.get(ApiUrl(`browser/start?user_id=${profile_id}`));
+                puppeteer = response.data.data.ws.puppeteer;
+                break;
+            } catch (error) {
+                console.log("Failed to launch browser Trying to run again...");
+            }
         }
+
+        res(puppeteer);
+
     });
 }
+
+//#endregion Scraper Functions 
+
 export {
-    CreateProfile,
-    startBrowser
+    wait,
+    between,
+    mergePath,
+    trim,
+    CreateProfile, // Create Profile
+    DeleteProfile, // Delete Profile
+    startBrowser, // Start Browser From Profile
 };
